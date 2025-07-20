2bbe56e58f0250c35639d56d216f8786ebca53d1
jayteemoney <jethroirmiya@gmail.com>
2025-07-20 15:25:02 +0000

    adjusted the hardhat file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    morph: {
+      url: process.env.MORPH_RPC,
+      accounts: [process.env.PRIVATE_KEY!]
+    }
+  }
 };
-
 export default config;
